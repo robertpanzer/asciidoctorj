22e0426a5793e66569e4ac522d69f61614adb87b
Robert Panzer <robert.panzer@me.com>
2015-08-02 16:56:28 +0200

    Fixed sed replacement for OSX

--- test-asciidoctor-upstream.sh
+++ test-asciidoctor-upstream.sh
@@ -20,7 +20,7 @@ cp ../../asciidoctor-gem-installer.pom $SRC_DIR/pom.xml
 cd $SRC_DIR
 ASCIIDOCTOR_VERSION=`grep 'VERSION' ./lib/asciidoctor/version.rb | sed "s/.*'\(.*\)'.*/\1/"`
 sed "s;<version></version>;<version>$ASCIIDOCTOR_VERSION</version>;" pom.xml > pom.xml.sedtmp && mv -f pom.xml.sedtmp pom.xml
-sed "s;^ *s\.files \+.*$;s.files = Dir['*.gemspec', '*.adoc', '{bin,data,lib}/*', '{bin,data,lib}/**/*'];" asciidoctor.gemspec > asciidoctor.gemspec.sedtmp && mv -f asciidoctor.gemspec.sedtmp asciidoctor.gemspec
+sed "s;^ *s\.files .*$;s.files = Dir['*.gemspec', '*.adoc', '{bin,data,lib}/*', '{bin,data,lib}/**/*'];" asciidoctor.gemspec > asciidoctor.gemspec.sedtmp && mv -f asciidoctor.gemspec.sedtmp asciidoctor.gemspec
 mvn install -Dgemspec=asciidoctor.gemspec
 cd ../..
 #rm -rf maven
